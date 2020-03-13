0a1424efd82a6376f33239c25a817bf65403744c
indiegamemaker <myleje@gmail.com>
2020-03-13 12:58:27 +0900

    Project Upload

---
+++ Assets/02.Scripts/ViveController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveController : MonoBehaviour
+{
+    public SteamVR_Input_Sources leftHand;
+    public SteamVR_Input_Sources any;
+    public SteamVR_Action_Boolean trigger;
+    public SteamVR_Action_Boolean trackPadTouch;
+    public SteamVR_Action_Vector2 trackPadPosition;
+
+    void Awake()
+    {
+        any = SteamVR_Input_Sources.Any;
+        leftHand = SteamVR_Input_Sources.LeftHand;
+        trigger = SteamVR_Actions.default_InteractUI;
+        trackPadTouch = SteamVR_Actions.default_TrackPadTouch;
+        trackPadPosition = SteamVR_Actions.default_TrackPadPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trigger.GetStateDown(leftHand))
+        {
+            Debug.Log("Trigger Button Clicked");
+        }
+
+        if (trackPadTouch.GetState(any))
+        {
+            Vector2 pos = trackPadPosition.GetAxis(any);
+            //Debug.LogFormat("TrackPad Position x={0}/y={1}", pos.x, pos.y);
+            Debug.Log($"Position x={pos.x} / y={pos.y}");
+        }
+    }
+}
