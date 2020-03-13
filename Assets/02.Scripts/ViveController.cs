a074c6c00d30d6503ca66aff1cc92bc3322a3037
indiegamemaker <myleje@gmail.com>
2020-03-13 13:09:48 +0900

    Update ViveController.cs

--- Assets/02.Scripts/ViveController.cs
+++ Assets/02.Scripts/ViveController.cs
@@ -33,6 +33,7 @@ public class ViveController : MonoBehaviour
             Vector2 pos = trackPadPosition.GetAxis(any);
             //Debug.LogFormat("TrackPad Position x={0}/y={1}", pos.x, pos.y);
 
+
             Debug.Log($"Position x={pos.x} / y={pos.y}"); 
         }
     }
