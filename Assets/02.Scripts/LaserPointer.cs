1b0f7bed37568fd0873a4fa9e69a1571e20dd38e
indiegamemaker <myleje@gmail.com>
2020-03-13 14:45:15 +0900

    /

---
+++ Assets/02.Scripts/LaserPointer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class LaserPointer : MonoBehaviour
+{
+    private SteamVR_Behaviour_Pose pose;
+    private SteamVR_Input_Sources hand;
+    private LineRenderer line;
+    public float distance = 10.0f;
+
+    void Start()
+    {
+        pose = GetComponent<SteamVR_Behaviour_Pose>();
+        hand = pose.inputSource;
+    }
+
+    void CreateLine()
+    {
+        line = this.gameObject.AddComponent<LineRenderer>();
+        line.useWorldSpace = false;
+        line.positionCount = 2;
+        line.SetPosition(0, Vector3.zero);
+        line.SetPosition(1, new Vector3(0, 0, distance));
+    }
+}
