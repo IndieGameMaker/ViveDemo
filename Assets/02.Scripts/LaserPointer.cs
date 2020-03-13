f601d1a718c155fdedb0774c525f4f88083078de
indiegamemaker <myleje@gmail.com>
2020-03-13 15:02:49 +0900

    /

--- Assets/02.Scripts/LaserPointer.cs
+++ Assets/02.Scripts/LaserPointer.cs
@@ -10,12 +10,26 @@ public class LaserPointer : MonoBehaviour
     private LineRenderer line;
     public float distance = 10.0f;
 
+    private Ray ray;
+    private RaycastHit hit;
+    private Transform tr;
+
     void Start()
     {
+        tr = GetComponent<Transform>();
         pose = GetComponent<SteamVR_Behaviour_Pose>();
         hand = pose.inputSource;
 
         CreateLine();
+    }
+
+    private void Update()
+    {
+        ray = new Ray(tr.position, tr.forward);
+        if (Physics.Raycast(ray, out hit, distance))
+        {
+            line.SetPosition(1, new Vector3(0, 0, hit.distance));
+        }
     }
 
     void CreateLine()
